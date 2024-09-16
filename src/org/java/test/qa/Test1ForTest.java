2b4113760f9080490fd644098d9f2fd869126697
admin <admin@DESKTOP-JAJI5O6>
2024-09-16 11:52:22 +0530

    My First Git Commit

---
+++ src/org/java/test/qa/Test1ForTest.java
@@ -0,0 +1,14 @@
+package org.java.test.qa;
+
+public class Test1ForTest {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Start---------");
+		
+		System.out.println("Abc");
+		
+		System.out.println("End---------");
+	}
+
+}
