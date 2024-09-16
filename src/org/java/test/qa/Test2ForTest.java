22c951c68c073dda38dcc130ca1fe8f496ba26b4
admin <admin@DESKTOP-JAJI5O6>
2024-09-16 12:33:26 +0530

    Added Test2ForTest class and commit.

---
+++ src/org/java/test/qa/Test2ForTest.java
@@ -0,0 +1,14 @@
+package org.java.test.qa;
+
+public class Test2ForTest {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Start---------");
+		
+		System.out.println("Abc");
+		System.out.println("Xyz");
+		System.out.println("End---------");
+	}
+
+}
